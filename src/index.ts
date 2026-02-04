baa1ee7dca2eefcf3fa922cc511f5a234b6984d7
palatA2000 <palat.tu@abbon.co.th>
2026-02-05 00:58:40 +0700

    vercel config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { Elysia } from 'elysia'
-import { config } from './config.js'
-import { lineWebhook } from './modules/line-webhook/index.js'
+import { Elysia } from "elysia";
+import { config } from "./config.js";
+import { lineWebhook } from "./modules/line-webhook/index.js";
 
-const app = new Elysia()
+export default new Elysia()
   .use(lineWebhook)
-  .get('/health', () => ({ ok: true }))
-  .listen(config.port)
-
-console.log(`LINE webhook server running on http://localhost:${config.port}`)
+  .get("/health", () => ({ ok: true }))
+  .listen(config.port);
